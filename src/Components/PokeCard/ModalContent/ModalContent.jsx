35e785b8d694c97652afb5febdf4e563beb5d175
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-08 12:27:41 -0400

    ajout de la modale

---
+++ src/Components/PokeCard/ModalContent/ModalContent.jsx
@@ -0,0 +1,6 @@
+import './ModalContent.css'
+export default function ModalContent() {
+  return (
+    <div className='ModalBox'>kcnikcfnjik</div>
+  )
+}
--- src/Components/PokeCard/PokeCard.jsx
+++ src/Components/PokeCard/PokeCard.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
+import { createPortal } from "react-dom"
+import ModalContent from './ModalContent/ModalContent'
+
 import './PokeCard.css'
 
 export default function PokeCard({ poke }) {
+    const [showModal, setShowModal] = useState(false)
     return (
         <>
             <li
-            className='PokeLi'
+            onClick={()=> setShowModal(!showModal)}
+                className='PokeLi'
             >
-               <h2 className='headingpoke'>{poke.name}</h2>
-               <div className="boxImg"><img src={poke.image} alt="Pokemon Image" /></div>
+                <h2 className='headingpoke'>{poke.name}</h2>
+                <div className="boxImg">
+                    <img src={poke.image} alt="Pokemon Image" />
+                </div>
             </li>
+            {showModal && createPortal(<ModalContent poke={poke} closeModal={() => setShowModal(!showModal)}/>, document.body)}
 
         </>
     )

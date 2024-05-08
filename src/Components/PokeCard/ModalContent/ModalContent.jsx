9cdafceb9d3ee6bdaf243165be59460888c32713
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-08 15:46:36 -0400

    modification modale

--- src/Components/PokeCard/ModalContent/ModalContent.jsx
+++ src/Components/PokeCard/ModalContent/ModalContent.jsx
@@ -1,6 +1,21 @@
 import './ModalContent.css'
-export default function ModalContent() {
+export default function ModalContent({poke, closeModal}) {
   return (
-    <div className='ModalBox'>kcnikcfnjik</div>
+    <div 
+    onClick={closeModal}
+    className='ModalBox'>
+      <div
+      onClick={e=> e.stopPropagation}
+      className="modalContent">
+        <button
+             onClick={closeModal}
+        >X</button>
+        <h1>{poke.name}</h1>
+        <p>Type :
+          <span>{poke.apiTypes.name}</span>
+        
+        </p>
+      </div>
+    </div>
   )
 }

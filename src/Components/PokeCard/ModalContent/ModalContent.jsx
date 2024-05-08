6bdccb6d900a7e5bb1a6ec68c044c9ee779fd969
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-08 16:15:20 -0400

    ajout du type des pokemon

--- src/Components/PokeCard/ModalContent/ModalContent.jsx
+++ src/Components/PokeCard/ModalContent/ModalContent.jsx
@@ -7,12 +7,12 @@ export default function ModalContent({poke, closeModal}) {
       <div
       onClick={e=> e.stopPropagation}
       className="modalContent">
-        <button
-             onClick={closeModal}
-        >X</button>
+     
         <h1>{poke.name}</h1>
+        <img src={poke.sprite} alt="" />
         <p>Type :
-          <span>{poke.apiTypes.name}</span>
+          <span>{poke.apiTypes[0].name}</span>
+          <img src={poke.apiTypes[0].image} alt="" />
         
         </p>
       </div>
